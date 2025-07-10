459c7037da42597d0f0ac8996c1e462ce31e121b
aminechairi <aminemesseiyah@gmail.com>
2025-07-10 15:50:37 +0100

    Modify Google authentication callback.

--- src/services/authServises/googleAuthService.js
+++ src/services/authServises/googleAuthService.js
@@ -12,9 +12,8 @@ exports.googleAuthCallback = asyncHandler(async (req, res) => {
 
   // Set the token as an HTTP-only, secure cookie
   res.cookie("JWTToken", token, {
-    domain: "https://www.eshopapp.shop", // allow subdomain sharing
     httpOnly: false,       // Accessible via JavaScript
-    secure: false,          // Only sent over HTTPS
+    secure: true,          // Only sent over HTTPS
     sameSite: "None",      // Allow cross-site usage
     maxAge: 90 * 24 * 60 * 60 * 1000, // Cookie expires in 90 days
   });
