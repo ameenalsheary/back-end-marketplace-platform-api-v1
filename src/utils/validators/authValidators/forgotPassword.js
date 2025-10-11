6d59e83ade6464301a445af0cfa634de136cd14c
aminechairi <aminemesseiyah@gmail.com>
2025-10-11 11:50:55 +0100

    Test.

--- src/utils/validators/authValidators/forgotPassword.js
+++ src/utils/validators/authValidators/forgotPassword.js
@@ -42,34 +42,3 @@ exports.passwordResetCodeValidator = [
 
   validatorMiddleware,
 ];
-
-//   check("email")
-//     .notEmpty()
-//     .withMessage("Email is required.")
-//     .isString()
-//     .withMessage("Email must be of type string.")
-//     .isEmail()
-//     .withMessage("Please provide a valid email address."),
-
-//   check("newPassword")
-//     .notEmpty()
-//     .withMessage("New password is required")
-//     .isString()
-//     .withMessage("New password must be of type string.")
-//     .isLength({ min: 8 })
-//     .withMessage("New password should be at least 8 characters long"),
-
-//   check("newPasswordConfirm")
-//     .notEmpty()
-//     .withMessage("New password confirm is required.")
-//     .isString()
-//     .withMessage("New password confirm must be of type string.")
-//     .custom((value, { req }) => {
-//       if (value !== req.body.newPassword) {
-//         throw new Error("New password confirm dose not match new password.");
-//       }
-//       return true;
-//     }),
-
-//   validatorMiddleware,
-// ];
