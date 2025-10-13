0c57d9d2be3d35d8da73bc4024efa0d8a393025a
aminechairi <aminemesseiyah@gmail.com>
2025-10-13 20:04:51 +0100

    Modify accessToken options.

--- src/services/authServises/authService.js
+++ src/services/authServises/authService.js
@@ -211,6 +211,8 @@ exports.verifySignIn = asyncHandler(async (req, res, next) => {
     httpOnly: true, // Prevents client-side JavaScript access
     secure: process.env.MODE_ENV === "production", // HTTPS only in production
     sameSite: process.env.MODE_ENV === "production" ? "None" : "Lax", // Cross-site requests allowed
+    domain: process.env.ALLOWED_ORIGINS.split(",")[0],
+    path: "/",
     maxAge: 90 * 24 * 60 * 60 * 1000, // 90 days expiration
   });
 
@@ -230,6 +232,8 @@ exports.logOut = asyncHandler(async (_, res) => {
     httpOnly: true,
     secure: process.env.MODE_ENV === "production",
     sameSite: process.env.MODE_ENV === "production" ? "None" : "Lax",
+    domain: process.env.ALLOWED_ORIGINS.split(",")[0],
+    path: "/",
     expires: new Date(0), // Expire immediately
   });
 
